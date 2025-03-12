12435a178b8e58f3fd627d38d391d3861cb5115f
charangoud2004 <k.charangoud2004@gmail.com>
2025-03-12 10:45:46 +0530

    Railway MySQL connection

--- src/routes/schoolRoutes.js
+++ src/routes/schoolRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const schoolController = require('../controllers/schoolController');
 
+router.get("/", (req, res) => {
+    res.send("Welcome to the School Management API");
+});
+
 router.post('/addSchool', schoolController.addSchool);
 router.get('/listSchools', schoolController.listSchools);
 
